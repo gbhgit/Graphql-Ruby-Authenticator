a8372c654104e19e7a8e325c0052aaf9ca3b0c1b
GabrielHolanda <gabrielbandeira@lapisco.ifce.edu.br>
2021-05-25 15:50:38 -0300

    [config] set to use initializer :fire: only in localhost

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,4 @@
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "your-production-app-here.com"
+# if Rails.end == "production"
+#    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "your-production-app-here.com"
+# end
+Rails.application.config.session_store :cookie_store, key: "_authentication_app"
